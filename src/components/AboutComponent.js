fd1337edd91f9ec5d53503636f7e59f97c9cedb8
zak-zaky <manulopez542@gmail.com>
2020-11-26 18:00:18 -0300

    Assignment 2 (fix)

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import RenderLeader from './RenderLeaderComponent';
+
+function RenderLeader({leader}) {
+
+    return(
+        <div key={leader.id} className="col-12 mt-5">
+            <Media tag="li">
+                <Media left middle>
+                    <Media object src={leader.image} alt={leader.name}/>
+                </Media>
+                <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>
+            </Media>
+        </div>
+    );
+
+}
 
 function About(props) {
 
